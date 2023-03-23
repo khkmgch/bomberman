5e0525e43f4c4903c2350c3e85a96a71917009fd
khkmgch <kohei.kamiguchi@gmail.com>
2023-03-23 18:58:31 +0900

    add : DTO

---
+++ server/src/game/dtos/BreakableObstacleDTO.ts
@@ -0,0 +1,15 @@
+import { GameObjectDTO } from './GameObjectDTO';
+
+export class BreakableObstacleDTO extends GameObjectDTO {
+  public id: number
+  // constructor(
+  //   private id: number,
+  //   x: number,
+  //   y: number,
+  //   size: number,
+  //   spriteKey: string,
+  //   animation: string,
+  // ) {
+  //   super(x, y, size, spriteKey, animation);
+  // }
+}
