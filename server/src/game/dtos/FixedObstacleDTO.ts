a01c559cf22fa588db3e8569ae7536a3f9dd5e95
khkmgch <kohei.kamiguchi@gmail.com>
2023-04-02 22:54:20 +0900

    update : DTOs

--- server/src/game/dtos/FixedObstacleDTO.ts
+++ server/src/game/dtos/FixedObstacleDTO.ts
@@ -1,15 +1,3 @@
 import { GameObjectDTO } from './GameObjectDTO';
 
-export class FixedObstacleDTO extends GameObjectDTO {
-  public id: number
-  // constructor(
-  //   private id: number,
-  //   x: number,
-  //   y: number,
-  //   size: number,
-  //   spriteKey: string,
-  //   animation: string,
-  // ) {
-  //   super(x, y, size, spriteKey, animation);
-  // }
-}
+export class FixedObstacleDTO extends GameObjectDTO {}
--- server/src/game/dtos/GameObjectDTO.ts
+++ server/src/game/dtos/GameObjectDTO.ts
@@ -1,14 +1,8 @@
 export abstract class GameObjectDTO {
+  public id: number;
   public x: number;
   public y: number;
   public size: number;
   public spriteKey: string;
   public animation: string;
-  // constructor(
-  //   protected x: number,
-  //   protected y: number,
-  //   protected size: number,
-  //   protected spriteKey: string,
-  //   protected animation: string,
-  // ) {}
 }
