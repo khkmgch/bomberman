31edf52f9ce766f756341233f28db6283a61ef03
khkmgch <kohei.kamiguchi@gmail.com>
2023-04-02 19:12:02 +0900

    update : type Objects

--- client/src/types/Objects.d.ts
+++ client/src/types/Objects.d.ts
@@ -1,8 +1,12 @@
+import { IBomb } from '../interfaces/IBomb';
 import { IBreakableObstacle } from '../interfaces/IBreakableObstacle';
 import { ICharacter } from '../interfaces/ICharacter';
 import { IEdgeObstacle } from '../interfaces/IEdgeObstacle';
+import { IExplosion } from '../interfaces/IExplosion';
 import { IFixedObstacle } from '../interfaces/IFixedObstacle';
 import { IGround } from '../interfaces/IGround';
+import { IItem } from '../interfaces/IItem';
+import { IMarker } from '../interfaces/IMarker';
 
 export type Objects = {
   characterMap: Map<number, ICharacter>;
@@ -10,22 +14,8 @@ export type Objects = {
   breakableObstacleMap: Map<number, IBreakableObstacle>;
   fixedObstacleMap: Map<number, IFixedObstacle>;
   edgeObstacleMap: Map<number, IEdgeObstacle>;
-  bombMap: {
-    [id: number]: {
-      sprite: Phaser.GameObjects.Sprite;
-      sync: BombDto | null;
-    };
-  };
-  explosionMap: {
-    [id: number]: {
-      sprite: Phaser.GameObjects.Sprite;
-      sync: ExplosionDto | null;
-    };
-  };
-  itemMap: {
-    [id: number]: {
-      sprite: Phaser.GameObjects.Sprite;
-      sync: ItemDto | null;
-    };
-  };
+  bombMap: Map<number, IBomb>;
+  markerMap: Map<number, IMarker>;
+  explosionMap: Map<number, IExplosion>;
+  itemMap: Map<number, IItem>;
 };
