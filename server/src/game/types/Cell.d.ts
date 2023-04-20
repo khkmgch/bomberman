0c3a40950cc9e0734516fe81a2b92d7322d466d4
khkmgch <kohei.kamiguchi@gmail.com>
2023-04-20 20:26:09 +0900

    update : type Cell

--- server/src/game/types/Cell.d.ts
+++ server/src/game/types/Cell.d.ts
@@ -1,7 +1,9 @@
 import { GameObject } from '../models/objects/GameObject';
 import { Item } from '../models/objects/item/Item';
+import { Index } from './Index';
 
 export type Cell = {
+  index: Index;
   entity: GameObject;
   item: Item;
 };
